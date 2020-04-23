45aecda6afdb5f4c9d37b1c001e220353a782df5
geldona <lap18002@byui.edu>
2020-04-23 11:12:09 -0600

    update

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-let currentdate = document.lastModified;
-        document.getElementById("modified").textContent = currentdate;
+function formatAMPM() {
+    var date = new Date();
+    var currDate = date.getDate();
+    var hours = date.getHours();
+    var dayName = getDayName(date.getDay());
+    var minutes = date.getMinutes();
+    var monthName = getMonthName(date.getMonth());
+    var year = date.getFullYear();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = dayName + ' ' + monthName + ' ' + currDate + ' ' + year + ' ' + hours + ':' + minutes + ' ' + ampm;
+    alert(strTime);
+}
+
+function getMonthName(month) {
+    var ar = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    return ar[month];
+}
