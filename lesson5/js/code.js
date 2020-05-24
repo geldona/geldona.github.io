8e8198a4b0267e614849d2ecb7b9988e5e91a099
geldona <lap18002@byui.edu>
2020-05-24 11:52:39 -0600

    fix

--- lesson5/js/code.js
+++ lesson5/js/code.js
@@ -14,19 +14,3 @@ let monthname  = month_name[mydate.getMonth()];
 const dateoutput = document.querySelector('.currentdate');
 dateoutput.textContent = weekname + ', ' + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
 
-//friday
-function fridays(){
-    console.log("daytime");
-     var full_date = new Date();
-     var daysofWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-     var dayofWeek = daysofWeek[full_date.getDay()]; 
-     if (dayofWeek == "Friday"){
-         let out = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
-         document.getElementById("fridays").style.display = "block"
-         document.getElementById("fridays").innerHTML = out;
-     }
-     else{
-         document.getElementById("fridays").style.display = "none"
-     }
- }
- fridays();
