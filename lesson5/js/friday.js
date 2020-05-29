8f315d2481a49d82c9e69c4016d6dcc81388b36f
geldona <lap18002@byui.edu>
2020-05-29 12:45:12 -0600

    fix

--- lesson5/js/friday.js
+++ lesson5/js/friday.js
@@ -5,7 +5,7 @@ function fridays(){
      var daysofWeek = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
      var dayofWeek = daysofWeek[full_date.getDay()]; 
      if (dayofWeek == "Friday"){
-         let out = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park center.";
+         let out = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the park.";
          document.getElementById("fridays").style.display = "block"
          document.getElementById("fridays").innerHTML = out;
      }
