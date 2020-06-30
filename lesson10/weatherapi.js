e8a63bdd3ddfc4e00131d097a631dc3aa777568f
geldona <lap18002@byui.edu>
2020-06-30 11:29:54 -0600

    push

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,15 @@
+const appid = '0b209e7419e85240602a2b16775256c6';
+const prestonId = '5604473'
+let url = `https://api.openweathermap.org/data/2.5/weather?id=${prestonId}&appid=${appid}`
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
