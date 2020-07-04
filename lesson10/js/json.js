fef0b76c143350f7e80cbf447a319dfd95aacb65
geldona <lap18002@byui.edu>
2020-07-04 17:02:32 -0600

    fix

--- lesson10/js/json.js
+++ lesson10/js/json.js
@@ -1,43 +1,90 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8cc80f462d0916f39d475a31d315920a"
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=810cde3d5910ebc0f4140c902d4ec7ff";
+weatherRequest.open("GET", apiURLstring, true);
+weatherRequest.responseType = "text";
+weatherRequest.send();
+weatherRequest.onload = function() {
+  let weatherData = JSON.parse(weatherRequest.responseText);
 
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject.list[0]);
+  document.getElementById("tempF").innerHTML = weatherData.main.temp + "&deg; F";
+  document.getElementById("high").innerHTML = weatherData.main.temp_max + "&deg; F";
+  document.getElementById("wind").innerHTML = weatherData.wind.speed + "mph";
+  document.getElementById("humid").innerHTML = weatherData.main.humidity + "%";
 
-        document.getElementById('current').textContent = jsObject.list[0].weather[0].main;
+  let factor = ((35.74 + 0.6215 * weatherData.main.temp) - (35.75 * Math.pow(weatherData.wind.speed,0.16)) + (0.4275 * weatherData.main.temp * Math.pow(weatherData.wind.speed,0.16))).toFixed(2);  
+  document.getElementById("windchill").innerHTML = factor + "&deg; F";
+}
 
-        document.getElementById('temp').textContent = ((jsObject.list[0].main.temp_max)*(9/5) - 459.67).toFixed(1);
+let forecastRequest = new XMLHttpRequest();
+let URLrequest = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=be03b0c014037eb9f061306615b7eb28";
+forecastRequest.open("GET", URLrequest, true);
+forecastRequest.responseType = "text";
+forecastRequest.send();
+forecastRequest.onload = function () {
+  let forecast = JSON.parse(forecastRequest.responseText);
+  let highTemp = [];
+  let day = 1;
+  let weekday = [];
+  var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
 
-        document.getElementById('humidity').textContent = jsObject.list[0].main.humidity;
+  for (let i = 0; i < forecast.list.length && day < 6; i++) {
+    if (forecast.list[i].dt_txt.includes("18:00:00")) {
+      highTemp[i] = forecast.list[i].main.temp;
+      let icon = "https://openweathermap.org/img/w/" + forecast.list[i].weather[0].icon + ".png";
+      let desc = forecast.list[i].weather[0].description;
+      let now = new Date(forecast.list[i].dt_txt);
+      weekday = days[now.getDay()];
+      document.getElementById("day" + day).innerHTML = weekday;
+      document.getElementById("forecast" + day).innerHTML = highTemp[i] + "&deg; F";
+      document.getElementById("ccimg" + day).setAttribute("src", icon);
+      document.getElementById("ccimg" + day).setAttribute("alt", desc);
+      day++;
+    }
+  }
+}
 
-        document.getElementById('windspeed').textContent = (jsObject.list[0].wind.speed).toFixed(0);    
-        })
+let requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+  let towndata = request.response;
+  showData(towndata);
+}
+function showData(jsonObj) {
+  let town = jsonObj["towns"];
 
-    .catch(function(error){
-        alert("Sorry, the data for the weather summary is not available right now.");
-    })
+  for (let i = 0; i < town.length; i++) {
+    if (town[i].name == "Preston") {
+      let article = document.getElementById("events");
+      let para1 = document.createElement("ul");
 
+      for (let a = 0; a < town[i].events.length; a++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = town[i].events[a];
+        para1.appendChild(listItem);
+      }
 
+      article.appendChild(para1);
 
-const apiFive = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8cc80f462d0916f39d475a31d315920a"
+    }
+  }
+}
 
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsonObject) => {
-        console.log(jsonObject);
+var now = new Date();
+var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+function fourdigits(number) {
+    return (number < 1000) ? number + 1900 : number;
+}
+today =  days[now.getDay()] + ", " +
+         date + " " +
+         months[now.getMonth()] + " " +
+         (fourdigits(now.getYear())) ;
+document.getElementById("currentdate").innerHTML = today;
 
-        for (let i = 1; i < 6; i++){
-            fore = document.textContent = jsonObject.list[i].main.temp;
-            convTemp = (fore * (9/5) - 459.67).toFixed(0);
-            document.getElementById('tempd' + i).textContent = convTemp;
-
-            imagesrc = jsonObject.list[i].weather[0].icon;
-            desc = jsonObject.list[i].weather[0].description;
-            console.log(jsonObject.list[i].weather[0].icon);
-
-            document.getElementById('icon' + i).setAttribute('src', 'http://openweathermap.org/img/w/' + imagesrc + '.png');
-            document.getElementById('icon' + i).setAttribute('alt', desc);
-        }}    
-    )
-
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
