025b2d4dd848f6bf51349cb929a845296a1ac2d2
geldona <lap18002@byui.edu>
2020-07-08 00:39:23 -0600

    fix gallery

--- lesson11/gallery/js/menu.js
+++ lesson11/gallery/js/menu.js
@@ -1,19 +1,17 @@
+var now = new Date();
+var days = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+function fourdigits(number) {
+    return (number < 1000) ? number + 1900 : number;
+}
+today =  days[now.getDay()] + ", " +
+         date + " " +
+         months[now.getMonth()] + " " +
+         (fourdigits(now.getYear())) ;
+document.getElementById("currentdate").innerHTML = today;
+
 function toggleMenu() {
-    document.getElementById('primaryNav').classList.toggle("hide")
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-const mydate = document.querySelector('.current-date');
-let d = new Date();
-let abbrw_names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thur'];
-let dayone = abbrw_names[d.getDay()+1];
-let daytwo = abbrw_names[d.getDay()+2];
-let daythree = abbrw_names[d.getDay()+3];
-let dayfour = abbrw_names[d.getDay()+4];
-let dayfive = abbrw_names[d.getDay()+5];
-console.log(dayfive);
-
-document.getElementById("day1").innerHTML = dayone;
-document.getElementById("day2").innerHTML = daytwo;
-document.getElementById("day3").innerHTML = daythree;
-document.getElementById("day4").innerHTML = dayfour;
-document.getElementById("day5").innerHTML = dayfive;
