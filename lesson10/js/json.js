2f7d4afbf5ca3f4d21933017a1fc5966f62d048e
geldona <lap18002@byui.edu>
2020-07-07 22:49:29 -0600

    fix

--- lesson10/js/json.js
+++ lesson10/js/json.js
@@ -16,3 +16,23 @@ document.getElementById("currentdate").innerHTML = today;
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+function showData(jsonObj) {
+  let town = jsonObj["towns"];
+
+  for (let i = 0; i < town.length; i++) {
+    if (town[i].name == "Preston") {
+      let article = document.getElementById("events");
+      let para1 = document.createElement("ul");
+
+      for (let a = 0; a < town[i].events.length; a++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = town[i].events[a];
+        para1.appendChild(listItem);
+      }
+
+      article.appendChild(para1);
+
+    }
+  }
+}
